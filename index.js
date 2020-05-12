f985a9080bde7961d70eb0273a9c6d1bad676e52
Jacob Silver <jacobsilver2@mac.com>
2020-05-12 16:38:59 +0000

    pushing to github after moving stuff to env variables

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-const cookieParser = require("cookie-parser");
-require("dotenv").config({ path: ".env" });
-const createServer = require("./createServer");
-const db = require("./db");
-const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config({ path: '.env' });
+const createServer = require('./createServer');
+const db = require('./db');
 
 const server = createServer();
 
 server.express.use(cookieParser());
 
-// decode the jwt so we can get the userid on each request
+
+// decode the JWT so we can get the user Id on each request
 server.express.use((req, res, next) => {
   const { token } = req.cookies;
   if (token) {
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
-    // put the userID onto the req for future requests to access
+    // put the userId onto the req for future requests to access
     req.userId = userId;
   }
   next();
 });
+
+// 2. Create a middleware that populates the user on each request
 
 server.express.use(async (req, res, next) => {
   // if they aren't logged in, skip this
   if (!req.userId) return next();
   const user = await db.query.user(
     { where: { id: req.userId } },
-    "{ id, permissions, email, name }"
+    '{ id, permissions, email, name }'
   );
   req.user = user;
   next();
 });
 
-const port = process.env.PORT || 3000;
+const whitelist = ['http://www.motowndb.com', 'https://www.motowndb.com']
 
-server.start(
-  port,
-  {
-    cors: {
-      credentials: true,
-      origin: process.env.FRONTEND_URL,
-    },
-  },
-  (deets) => {
+server.start({
+  cors: {
+	credentials: true,
+	origin: ['http://www.motowndb.com', 'https://www.motowndb.com']
+}
+},
+  deets => {
+	console.log(deets)
     console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
+
